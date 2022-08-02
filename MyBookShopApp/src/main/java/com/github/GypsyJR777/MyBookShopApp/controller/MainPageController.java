a9a17de59127f688f157267936bcddb32070ecfd
GypsyJR777 <ivan.demidov.01@list.ru>
2022-08-02 04:39:29 +0300

    replace templates

--- MyBookShopApp/src/main/java/com/github/GypsyJR777/MyBookShopApp/controller/MainPageController.java
+++ MyBookShopApp/src/main/java/com/github/GypsyJR777/MyBookShopApp/controller/MainPageController.java
@@ -1,15 +1,20 @@
 package com.github.GypsyJR777.MyBookShopApp.controller;
 
+import com.github.GypsyJR777.MyBookShopApp.entity.Book;
 import com.github.GypsyJR777.MyBookShopApp.service.AuthorService;
 import com.github.GypsyJR777.MyBookShopApp.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 @Controller
-@RequestMapping("/bookshop")
 public class MainPageController {
     private final BookService bookService;
     private final AuthorService authorService;
@@ -20,9 +25,19 @@ public class MainPageController {
         this.authorService = authorService;
     }
 
-    @GetMapping("/main")
-    public String mainPage(Model model) {
-        model.addAttribute("bookData", bookService.getBooksData());
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks() {
+        return bookService.getBooksData();
+    }
+
+    @GetMapping("/")
+    public String mainPage() {
+//        model.addAttribute("bookData", bookService.getBooksData());
+//        model.addAttribute("searchPlaceholder", "new search placeholder");
+//        model.addAttribute("serverTime", new SimpleDateFormat("hh:mm:ss").format(new Date()));
+//        model.addAttribute("placeholderTextPart2", "SERVER");
+//        model.addAttribute("messageTemplate", "searchbar.placeholder2");
+
         return "index";
     }
 
